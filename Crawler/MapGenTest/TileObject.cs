9d6bf1d926203cfca583793649166af40424653f
Hailey <hailey@not_an_email.fake>
2023-12-09 15:34:36 -0600

    Added tile object

---
+++ Crawler/MapGenTest/TileObject.cs
@@ -0,0 +1,53 @@
+﻿using SDL2;
+
+namespace MapGenTest;
+
+internal class TileObject
+{
+    public SDL.SDL_Point Point;
+    public Sprite Sprite;
+
+    public void Move(Direction direction, int spaces)
+    {
+        switch (direction)
+        {
+            case Direction.S:
+                Point.y += spaces;
+                break;
+
+            case Direction.N:
+                Point.y -= spaces;
+                break;
+
+            case Direction.E:
+                Point.x -= spaces;
+                break;
+
+            case Direction.W:
+                Point.x += spaces;
+                break;
+
+            case Direction.NE:
+                Move(Direction.N, spaces);
+                Move(Direction.E, spaces);
+                break;
+
+            case Direction.NW:
+                Move(Direction.N, spaces);
+                Move(Direction.W, spaces);
+                break;
+
+            case Direction.SE:
+                Move(Direction.S, spaces);
+                Move(Direction.E, spaces);
+                break;
+
+            case Direction.SW:
+                Move(Direction.S, spaces);
+                Move(Direction.W, spaces);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+        }
+    }
+}
